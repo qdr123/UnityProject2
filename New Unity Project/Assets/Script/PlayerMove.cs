5649c3272aaaaadae950902358ff01e4a1e61132
qdr123 <tmxjdydrb12@gmail.com>
2020-06-15 15:40:51 +0900

    팔로우 카메라 카메라 회전처리 플레이어 이동 충돌처리

---
+++ New Unity Project/Assets/Script/PlayerMove.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    Vector3 pos;
+    public float moveSpeed = 10.0f;
+    CharacterController cc; //캐릭터컨트롤러 컴포넌트
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+       
+    }
+
+    private void Move()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        pos = (Vector3.forward * v) + (Vector3.right * h);
+        //pos.Normalize();//대각선이동 속도를 상
+        this.transform.Translate(pos * moveSpeed * Time.deltaTime);
+
+        pos = Camera.main.transform.TransformDirection(pos);
+        //transform.Translate(pos * moveSpeed * Time.deltaTime)
+        //심각한 문제 : 하늘 날라다님 , 땅 뚫음, 충돌처리 안됨
+        //캐릭터컨트롤러를 사용한다.
+        // 
+
+        cc.Move(pos * moveSpeed * Time.deltaTime);
+    }
+}
---
+++ New Unity Project/Assets/Script/PlayerRotate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotate : MonoBehaviour
+{
+    //각도 조절시 값을 크게 
+    public float speed = 150; //회전속도(Time.DeltaTime을 통해 1초에 150도 회전)
+
+    float angleX, angleY;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotate();
+    }
+
+    private void Rotate()
+    {
+        float h = Input.GetAxis("Mouse X");
+        angleX += h * speed * Time.deltaTime;
+        transform.eulerAngles = new Vector3(0, angleX, 0);
+
+       
+
+    }
+}
